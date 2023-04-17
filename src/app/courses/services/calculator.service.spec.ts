945a9ef5ce3e62a49b038a6e26f46d8288fd6e50
Ambruspal <phaal83@gmail.com>
2023-04-17 16:45:52 +0200

    Day 1

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,24 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
+import { CalculatorService } from "./calculator.service";
 
+describe("CalculatorService", () => {
+  let logger: any;
+  let calculatorService: CalculatorService;
 
-describe('CalculatorService', () => {
+  beforeEach(() => {
+    logger = jasmine.createSpyObj("LoggerService", ["log"]);
+    calculatorService = new CalculatorService(logger);
+  });
 
-    let calculator: CalculatorService,
-        loggerSpy: any;
+  it("should add two numbers", () => {
+    const result = calculatorService.add(2, 4);
 
-    beforeEach(()=> {
+    expect(result).toBe(6);
+    expect(logger.log).toHaveBeenCalledTimes(1);
+  });
 
-        console.log("Calling beforeEach");
+  it("should subtract two numbers", () => {
+    const result = calculatorService.subtract(7, 3);
 
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
-
-        TestBed.configureTestingModule({
-            providers: [
-                CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
-            ]
-        });
-
-        calculator = TestBed.get(CalculatorService);
-
-    });
-
-    it('should add two numbers', () => {
-
-        console.log("add test");
-
-        const result = calculator.add(2, 2);
-
-        expect(result).toBe(4);
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
-
-
-    it('should subtract two numbers', () => {
-
-        console.log("subtract test");
-
-        const result = calculator.subtract(2, 2);
-
-        expect(result).toBe(0, "unexpected subtraction result");
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
-    });
-
+    expect(result).toBe(4, "unexpected subtraction result");
+  });
 });
