615c84a9b45f056dc6596165bce9078b838a46f1
Ambruspal <phaal83@gmail.com>
2023-05-14 15:10:19 +0200

    Http kérések tesztje 1.

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,19 +1,32 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
-  let logger: any;
+  let loggerSpy: any;
   let calculatorService: CalculatorService;
 
   beforeEach(() => {
-    logger = jasmine.createSpyObj("LoggerService", ["log"]);
-    calculatorService = new CalculatorService(logger);
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {
+          provide: LoggerService,
+          useValue: loggerSpy,
+        },
+      ],
+    });
+
+    calculatorService = TestBed.get(CalculatorService);
   });
 
   it("should add two numbers", () => {
     const result = calculatorService.add(2, 4);
 
     expect(result).toBe(6);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
